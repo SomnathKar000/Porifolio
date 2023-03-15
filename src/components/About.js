b4860c9c5007b733382808a517d688a3302839bd
SomnathKar000 <somnathkar201901@gmail.com>
2023-03-15 21:54:32 +0530

    Added new changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,77 @@
-import { Typography, Box, Container, Button } from "@mui/material";
 import React from "react";
+import Somnath from "../assets/Somnath.jpeg";
 import styled from "styled-components";
+import { Typography, Box, Container, Button } from "@mui/material";
 import { Contextvalues } from "../context/context";
 
 const About = () => {
+  console.log(window.location);
   return (
-    <Container maxWidth="xl">
-      <Box>
-        <Typography variant="h3">
-          Hi, I'm Somnath
-          <br /> I love to build amazing apps
-        </Typography>
-        <Typography variant="p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam neque
-          rem amet dicta unde debitis consectetur hic velit ipsa nesciunt! Lorem
-          ipsum dolor sit amet consectetur, adipisicing elit. Odio soluta nisi
-          doloremque
-        </Typography>
-        <Box>
-          <Button sx={{ margin: "1rem" }} variant="contained" color="success">
-            Contact me
-          </Button>
-          <Button variant="contained">See my projects</Button>
+    <Wrapper>
+      <Container className="Contain" maxWidth="xl">
+        <Box classname="text_box">
+          <Typography variant="h3">
+            Hi, I'm Somnath
+            <br /> I love to build amazing apps
+          </Typography>
+          <Typography variant="p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam neque
+            rem amet dicta unde debitis consectetur hic velit ipsa nesciunt!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
+            soluta nisi doloremque
+          </Typography>
+          <Box>
+            <Button sx={{ margin: "1rem" }} variant="contained" color="success">
+              Contact me
+            </Button>
+            <Button variant="contained">See my projects</Button>
+          </Box>
         </Box>
-      </Box>
-      <Box>
-        <img src="../assets/Somnath.jpeg" alt="Somnath" />
-      </Box>
-    </Container>
+        <Box className="profile_box">
+          <img
+            calssName="profile_box"
+            src={Somnath}
+            alt="Somnath"
+            width="70%"
+          />
+        </Box>
+      </Container>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  img {
+    border-radius: 10%;
+  }
+  margin: 5rem 1rem;
+  .Contain {
+    display: grid;
+    grid-template-columns: repeat(2, 2fr);
+    grid-template-areas:
+      "profile_box profile_box"
+      "text_box text_box";
+  }
+  .profile_box {
+    grid-area: profile_box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .text_box {
+    grid-area: text_box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 1rem;
+  }
+  @media screen and (min-width: 900px) {
+    .Contain {
+      grid-template-areas:
+        "profile_box text_box"
+        "profile_box text_box";
+    }
+  }
+`;
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import {
+  Typography,
+  Box,
+  Container,
+  Button,
+  ImageList,
+  ImageListItem,
+} from "@mui/material";
+import photo from "../assets/photo.avif";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const images = [photo, photo, photo, photo];
+  return (
+    <>
+      <Container>
+        <Box>
+          <AccountTreeIcon sx={{ fontSize: 40 }} />
+          <Typography variant="h3">Apps I've Build</Typography>
+        </Box>
+        <Box>
+          <ImageList sx={{ height: 450, width: 500 }} cols={2}>
+            {images.map((item, index) => (
+              <ImageListItem key={index}>
+                <img src={item}></img>
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default Projects;
